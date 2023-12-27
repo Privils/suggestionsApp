f0ac748138905d4f21236b15d199a5cbab5a4198
Privils <privilledgeblack8@gmail.com>
2023-12-28 00:07:43 +0200

    added th music file but still requires the styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Music from './components/Music';
+import Anime from './components/Anime';
+import Movies from './components/Movies'
 
 const provider = createBrowserRouter(
   createRoutesFromElements(
     <Route path='suggestionsApp' element={<Header />}>
       <Route index element={<Home/>}/>
+      <Route path='Music' element={<Music />}/>
+      <Route path='Anime' element={<Anime />}/>
     </Route>
   )
 )
